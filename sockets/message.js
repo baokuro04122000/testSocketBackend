9941b82216c214b8928eb84213d4698c5b1bd8b6
dinhbao <baotrandinh100@gmail.com>
2021-06-19 13:16:40 +0700

    fix few bug on server

--- sockets/message.js
+++ sockets/message.js
@@ -3,6 +3,7 @@ const messageSocket = (io)=>{
     io.on('connection', (socket) => {
         clients.push(socket.id)
         io.emit('allUsers',{clients})
+        console.log(clients);
         socket.on('disconnect', () => {
           clients = clients.filter(user=>user !== socket.id)
         })
