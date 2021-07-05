9377020c129a75af2dc9b6f692111c30d1eaf268
baotran13 <bao.tran@techupcorp.com>
2021-07-05 16:18:53 +0700

    up

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin:*');
+    res.header('Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS');
     next();
 })
 
